71a04e40186d8fd8b60a47314b666e1aa36ebbde
Justin Lazarski <blackhawks189@gmail.com>
2018-04-23 13:30:59 -0700

    Finished the Content of Lecture 2 on MVC design. I still need to build a card shuffler

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -11,14 +11,29 @@ import Foundation
 class Concentration
 {
     var cards = [Card]()
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
   
-    func chooseCard(at index: Int)
-    {
-        if cards[index].isFaceUp {
-            cards[index].isFaceUp = false
-        } else {
-            cards[index].isFaceUp = true
- 
+    func chooseCard(at index: Int) {
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                // Check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                    
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+                
+            } else {
+                // Either no cards or two cards are face up
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
         }
     }
     init(numberOfPairsOfCards: Int) {
@@ -26,7 +41,7 @@ class Concentration
             let card = Card()
            cards += [card, card]
         }
-        // TODO shuffle the cards
+        // TODO: Shuffle the cards
         
     }
 }
